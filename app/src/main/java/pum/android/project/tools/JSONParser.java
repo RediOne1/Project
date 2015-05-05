3b4eb86692f7ee82ff29b769f9618324ce08b58f
Adrian Kuta <redione193@gmail.com>
2015-05-05 10:17:25 +0200

    init

---
+++ app/src/main/java/pum/android/project/tools/JSONParser.java
@@ -0,0 +1,103 @@
+package pum.android.project.tools;
+
+import android.util.Log;
+
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.NameValuePair;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.client.utils.URLEncodedUtils;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.params.BasicHttpParams;
+import org.apache.http.params.HttpConnectionParams;
+import org.apache.http.params.HttpParams;
+import org.apache.http.protocol.HTTP;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.List;
+
+import pum.android.project.BuildConfig;
+
+public class JSONParser {
+
+    static InputStream is = null;
+    static JSONObject jObj = null;
+    static String json = null;
+
+    // constructor
+    public JSONParser() {
+
+    }
+
+    // function get json from url
+    // by making HTTP POST or GET mehtod
+    public JSONObject makeHttpRequest(String url, String method,
+                                      List<NameValuePair> params) throws IOException {
+
+        // Making HTTP request
+
+        HttpParams httpParameters = new BasicHttpParams();
+        HttpConnectionParams.setConnectionTimeout(httpParameters, 3000);
+        HttpConnectionParams.setSoTimeout(httpParameters, 5000);
+        // check for request method
+        if (method == "POST") {
+            // request method is POST
+            // defaultHttpClient
+            DefaultHttpClient httpClient = new DefaultHttpClient(httpParameters);
+            HttpPost httpPost = new HttpPost(url);
+            httpPost.setEntity(new UrlEncodedFormEntity(params, HTTP.UTF_8));
+            HttpResponse httpResponse = httpClient.execute(httpPost);
+            HttpEntity httpEntity = httpResponse.getEntity();
+            is = httpEntity.getContent();
+
+        } else if (method == "GET") {
+            // request method is GET
+            DefaultHttpClient httpClient = new DefaultHttpClient(httpParameters);
+            if (params != null) {
+                String paramString = URLEncodedUtils.format(params, HTTP.UTF_8);
+                url += "?" + paramString;
+            }
+            HttpGet httpGet = new HttpGet(url);
+            httpGet.addHeader("Cache-Control", "no-cache no-store");
+            HttpResponse httpResponse = httpClient.execute(httpGet);
+            httpResponse.addHeader("Cache-Control", "no-cache no-store");
+            HttpEntity httpEntity = httpResponse.getEntity();
+            is = httpEntity.getContent();
+        }
+        try {
+            BufferedReader reader = new BufferedReader(new InputStreamReader(is, "utf-8"), 10000);
+            StringBuilder sb = new StringBuilder();
+            String line = null;
+            while ((line = reader.readLine()) != null) {
+                sb.append(line + "\n");
+            }
+            //TODO Usun to przed wydaniem apki.
+            if(BuildConfig.DEBUG)
+            Log.d("InputStream", sb.toString());
+            is.close();
+            json = sb.toString();
+        } catch (Exception e) {
+            Log.e("Buffer Error", "Error converting result " + e.toString());
+        }
+
+        // try parse the string to a JSON object
+        try {
+            if (json == null)
+                throw new JSONException("");
+            jObj = new JSONObject(json);
+        } catch (JSONException e) {
+            Log.e("JSON Parser", "Error parsing data " + e.toString());
+        }
+
+        // return JSON String
+        return jObj;
+
+    }
+}
