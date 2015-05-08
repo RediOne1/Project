6f8e0c92dd856c6d39c28c06b51597109bcbd69e
Adrian Kuta <redione193@gmail.com>
2015-05-08 21:11:19 +0200

    CardView

--- app/src/main/java/pum/android/project/RecipeTab/RecipesFragment.java
+++ app/src/main/java/pum/android/project/RecipeTab/RecipesFragment.java
@@ -1,12 +1,12 @@
 package pum.android.project.RecipeTab;
 
 import android.app.Fragment;
-import android.app.ListFragment;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ListView;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -18,10 +18,12 @@ import pum.android.project.tools.Recipe;
 /**
  * A simple {@link Fragment} subclass.
  */
-public class RecipesFragment extends ListFragment {
+public class RecipesFragment extends Fragment{
 
-	private RecipeListAdapter adapter;
 	private List<Recipe> recipeList;
+	private RecyclerView mRecyclerView;
+	private RecyclerView.Adapter mAdapter;
+	private RecyclerView.LayoutManager mLayoutManager;
 
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -35,14 +37,26 @@ public class RecipesFragment extends ListFragment {
 		super.onViewCreated(view, savedInstanceState);
 
 		recipeList = new ArrayList<>();
-		ListView lv = getListView();
-		adapter = new RecipeListAdapter(getActivity(), recipeList);
-		lv.setAdapter(adapter);
+
+		mRecyclerView = (RecyclerView) view.findViewById(R.id.my_recycler_view);
+
+		// use this setting to improve performance if you know that changes
+		// in content do not change the layout size of the RecyclerView
+		mRecyclerView.setHasFixedSize(true);
+
+		// use a linear layout manager
+		mLayoutManager = new LinearLayoutManager(getActivity());
+		mRecyclerView.setLayoutManager(mLayoutManager);
+
+		// specify an adapter (see also next example)
+		mAdapter = new MyAdapter(recipeList);
+		mRecyclerView.setAdapter(mAdapter);
+
 		recipeList.add(new Recipe("Cycki"));
 		recipeList.add(new Recipe("Więcej cycków"));
 		recipeList.add(new Recipe("Jeszcze więcej cycków"));
 		recipeList.add(new Recipe("O matko!"));
 		recipeList.add(new Recipe("I tatko!"));
-		adapter.notifyDataSetChanged();
+		mAdapter.notifyDataSetChanged();
 	}
 }
