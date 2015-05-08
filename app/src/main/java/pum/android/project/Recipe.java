ca9002039b69faab070db820e588719261f770b5
Adrian Kuta <redione193@gmail.com>
2015-05-08 18:40:31 +0200

    Dodano obiekt, Przepis(Recipe)

---
+++ app/src/main/java/pum/android/project/Recipe.java
@@ -0,0 +1,18 @@
+package pum.android.project;
+
+import java.util.List;
+
+/**
+ * author:  Adrian Kuta
+ * index:   204423
+ * date:    08.05.15
+ */
+public class Recipe {
+
+	public long id;
+	public String name;
+	public List<String> images;
+	public String ingredients;
+	public String description;
+
+}
