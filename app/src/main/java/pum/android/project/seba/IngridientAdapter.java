7ca1c53a6d3965562a1b226ebfb922c1af6d8c37
Shtepi <sebastian.brzezinka@gmail.com>
2015-05-18 15:49:36 +0200

    Niedziałające onClickItem

--- app/src/main/java/pum/android/project/seba/IngridientAdapter.java
+++ app/src/main/java/pum/android/project/seba/IngridientAdapter.java
@@ -5,6 +5,8 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
 import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import java.util.List;
 
@@ -15,19 +17,22 @@ import pum.android.project.R;
  */
 public class IngridientAdapter extends BaseAdapter
 {
+    private List<String> ingList;
     private LayoutInflater inflater;
-    public IngridientAdapter(Context context){
+
+    public IngridientAdapter(Context context, List<String> ingList){
+        this.ingList=ingList;
         inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
     }
 
     @Override
     public int getCount() {
-        return 0;
+        return ingList.size();
     }
 
     @Override
     public String getItem(int position) {
-        return "0";
+        return ingList.get(position);
     }
 
     @Override
@@ -37,6 +42,13 @@ public class IngridientAdapter extends BaseAdapter
 
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
+        if(convertView == null) {
+          convertView = inflater.inflate(R.layout.ingridient, null);
+        }
+        ImageView imageView = (ImageView) convertView.findViewById(R.id.imageView);
+        imageView.setBackgroundColor(00000);
+        TextView title = (TextView) convertView.findViewById(R.id.textView);
+        title.setText(getItem(position));
         return  convertView;
     }
 }
--- app/src/main/java/pum/android/project/seba/LodowkaFragment.java
+++ app/src/main/java/pum/android/project/seba/LodowkaFragment.java
@@ -7,15 +7,23 @@ import android.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.Button;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
 
 import pum.android.project.R;
 
 /**
  * A simple {@link Fragment} subclass.
  */
-public class LodowkaFragment extends ListFragment implements View.OnClickListener {
-
-
+public class LodowkaFragment extends ListFragment implements View.OnClickListener,  AdapterView.OnItemLongClickListener {
+    private static ListView lv;
+    private static ArrayList<String> ingList;
+    private static IngridientAdapter ingridientAdapter;
+    int i=0;
 	public LodowkaFragment() {
 		// Required empty public constructor
 	}
@@ -24,9 +32,41 @@ public class LodowkaFragment extends ListFragment implements View.OnClickListene
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 		// Inflate the layout for this fragment
-		return inflater.inflate(R.layout.fragment_lodowka, container, false);
+        View view =inflater.inflate(R.layout.fragment_lodowka, container, false);
+        return view;
 	}
-    public void onClick(View v){
+    @Override
+    public void onActivityCreated(Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        Button addButton = (Button) getActivity().findViewById(R.id.add_button);
+        addButton.setOnClickListener(this);
+        lv = getListView();
+        ingList = new ArrayList<>();
+        ingridientAdapter = new IngridientAdapter(getActivity(),ingList);
+        lv.setAdapter(ingridientAdapter);
+        lv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
 
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+              Toast.makeText(getActivity(),"asdasd",Toast.LENGTH_SHORT).show();
+            }
+        });
+        lv.setOnItemLongClickListener(this);
+        ingList.add("cos");
+        ingList.add("cos2");
+        ingList.add("cos3");
+        ingridientAdapter.notifyDataSetChanged();
+
+    }
+
+
+    public void onClick(View v){
+        Toast.makeText(getActivity(),"sad"+Integer.toString(i),Toast.LENGTH_SHORT).show();
+    }
+    @Override
+    public boolean onItemLongClick(AdapterView<?> adapterView, View view, int i, long l) {
+        Toast.makeText(getActivity(),"sad2",Toast.LENGTH_SHORT).show();
+
+        return false;
     }
 }
